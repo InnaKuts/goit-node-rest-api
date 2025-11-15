d68c543c72d2a5a5841345d10503e7e3a4fad6b7
Inna Kuts <inna.v.kuts@gmail.com>
2025-11-15 11:47:23 +0100

    moved local db to other folder

--- helpers/paths.js
+++ helpers/paths.js
@@ -3,7 +3,7 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.resolve(path.dirname(__filename), "..");
 
-const contactsPath = path.join(__dirname, "db", "contacts.json");
+const contactsPath = path.join(__dirname, ".db", "contacts.json");
 const seedPath = path.join(__dirname, "seed.json");
 
 export { contactsPath, seedPath };
