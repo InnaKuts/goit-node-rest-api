be52117bf78b0f258f43579bab878a47b03c185d
Inna Kuts <inna.v.kuts@gmail.com>
2025-11-15 11:43:26 +0100

    seed

---
+++ seed.sql
@@ -0,0 +1,12 @@
+INSERT INTO contacts (id, name, email, phone, favorite, created_at, updated_at) VALUES
+('AeHIrLTr6JkxGE6SN-0Rw','Allen Raymond','nulla.ante@vestibul.co.uk','(992) 914-3792', false, NOW(), NOW()),
+('qdggE76Jtbfd9eWJHrssH','Chaim Lewis','dui.in@egetlacus.ca','(294) 840-6685', false, NOW(), NOW()),
+('drsAJ4SHPYqZeG-83QTVW','Kennedy Lane','mattis.Cras@nonenimMauris.net','(542) 451-7038', false, NOW(), NOW()),
+('vza2RIzNGIwutCVCs4mCL','Wylie Pope','est@utquamvel.net','(692) 802-2949', false, NOW(), NOW()),
+('05olLMgyVQdWRwgKfg5J6','Cyrus Jackson','nibh@semsempererat.com','(501) 472-5218', false, NOW(), NOW()),
+('1DEXoP8AuCGYc1YgoQ6hw','Abbot Franks','scelerisque@magnis.org','(186) 568-3720', false, NOW(), NOW()),
+('Z5sbDlS7pCzNsnAHLtDJd','Reuben Henry','pharetra.ut@dictum.co.uk','(715) 598-5792', false, NOW(), NOW()),
+('C9sjBfCo4UJCWjzBnOtxl','Simon Morton','dui.Fusce.diam@Donec.com','(233) 738-2360', false, NOW(), NOW()),
+('e6ywwRe4jcqxXfCZOj_1e','Thomas Lucas','nec@Nulla.com','(704) 398-7993', false, NOW(), NOW()),
+('rsKkOQUi80UsgVPCcLZZW','Alec Howard','Donec.elementum@scelerisquescelerisquedui.net','(748) 206-2688', false, NOW(), NOW())
+ON CONFLICT DO NOTHING;
