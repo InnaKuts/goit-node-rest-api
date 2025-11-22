9346828f56007ef5dd3f94b0e5b57cc7f0ea5a3c
Inna Kuts <inna.v.kuts@gmail.com>
2025-11-22 11:50:13 +0100

    migration contacts

---
+++ migrations/20251122104737-create-contacts-table.js
@@ -0,0 +1,45 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("contacts", {
+      id: {
+        type: Sequelize.STRING(255),
+        primaryKey: true,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+      },
+      phone: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+      },
+      favorite: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+        allowNull: true,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("contacts");
+  },
+};
