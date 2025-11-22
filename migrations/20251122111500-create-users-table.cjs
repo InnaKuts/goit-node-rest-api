927cff040ebd5032c193eb6a1c61a2294561c9d7
Inna Kuts <inna.v.kuts@gmail.com>
2025-11-22 12:13:13 +0100

    user migration

---
+++ migrations/20251122111500-create-users-table.cjs
@@ -0,0 +1,48 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("users", {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+        unique: true,
+      },
+      subscription: {
+        type: Sequelize.ENUM("starter", "pro", "business"),
+        defaultValue: "starter",
+        allowNull: false,
+      },
+      token: {
+        type: Sequelize.STRING(255),
+        defaultValue: null,
+        allowNull: true,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("users");
+  },
+};
